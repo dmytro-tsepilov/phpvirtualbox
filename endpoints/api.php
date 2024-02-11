19699d17491926cbaff901b7a3140da818ea166f
Dmytro Tsepilov <dmytro.tsepilov@gmail.com>
2024-02-11 15:00:48 +0000

    About box - added php version

--- endpoints/api.php
+++ endpoints/api.php
@@ -102,6 +102,8 @@ try {
 			$response['data']['responseData']['groupDefinitionKey'] = ($settings->phpVboxGroups ? vboxconnector::phpVboxGroupKey : 'GUI/GroupDefinitions');
 
 			$response['data']['success'] = true;
+			// Add php version
+			$response['data']['responseData']['version']['php'] = phpversion();
 
 			break;
 
